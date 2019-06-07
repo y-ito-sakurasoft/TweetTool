51aa6c2ee96f0d8d34a6f04704d2ab8391525c43
y-ito-sakurasoft <y.ito@sakurasoft.co.jp>
2019-06-07 13:37:01 +0900

    つぶやきツール作成

--- app/index.js
+++ app/index.js
@@ -4,3 +4,16 @@ var server = require("http").createServer(function (req, res) {
     var output = fs.readFileSync(__dirname + '/index.html', "utf-8");
     res.end(output);
 }).listen(3000);
+
+var io = require("socket.io").listen(server);
+
+io.sockets.on("connection", function (socket) {
+
+    socket.on("connected", function () { });
+
+    socket.on("publish", function (data) {
+        io.sockets.emit("publish", data);
+    });
+
+    socket.on("disconnect", function () { });
+});
